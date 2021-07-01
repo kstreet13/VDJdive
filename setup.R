c3b16c7a3380ac85dc59f04198fe14bbad0e731d
Kelly Street <kstreet@root-pcs-MacBook-Air.local>
2021-07-01 16:53:42 -0400

    added script for making toy data (on kraken) and for adding TCR data from file to an existing SCE object (still needs some testing, but seems to mostly work)

---
+++ setup.R
@@ -0,0 +1,33 @@
+
+# add TCR (or similar) data to SCE
+
+#dir <- '~/Projects/rcc/Data/TCR/samples'
+#sce <- loadHDF5SummarizedExperiment('~/Projects/rcc/sceMNN')
+
+# future work: make it more flexible so that 'sce' can be a SCE object or a directory,
+#    use DropletUtils::read10xCounts to get SCE from directory
+
+# need to figure out barcodes. Why are they all *_1 in the TCR data? is replacing that with ii really doing the matching properly? (~94%)
+
+require(S4Vectors)
+addTCRtoSCE(dir, sce){
+    # get TCR contigs
+    sample_ids <- system(paste('ls',dir), intern = TRUE)
+    contigs <- NULL
+    for (ii in 1:length(sample_ids)) {
+        tcr_ii <- read.csv(file.path(dir, sample_ids[ii],
+                                  "filtered_contig_annotations.csv"))
+        tcr_ii$barcode <- gsub("1$", ii, tcr_ii$barcode)
+        tcr_ii$type <- "TCR"
+        tcr_ii$clonotype_tcr <- paste0(sample_ids[ii], "-", tcr_ii$raw_clonotype_id)
+        tcr_ii$sample <- sample_ids[ii]
+        contigs <- rbind(contigs, tcr_ii)
+    }
+    rm(ii, tcr_ii)
+    # convert to 
+    tcr.list <- split(DataFrame(contigs), factor(contigs$barcode, sce$cell))
+    class(tcr.list)
+    
+    sce$contigs <- tcr.list
+    return(sce)
+}
