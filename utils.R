a878455d0599bde86f7bdc2fca36a80e1f5fb1b1
Kelly Street <kstreet@root-pcs-MacBook-Air.local>
2021-09-27 16:48:12 -0400

    added utils.R for general-use functions. Currently includes functions for splitting a clonotype counts matrix by a factor variable and summarizing a clonotype counts matrix at levels determined by a factor variable.

---
+++ utils.R
@@ -0,0 +1,53 @@
+source('EMquant.R')
+sce <- readRDS('~/Desktop/toyTCRdata.rds')
+sce <- EMquant(sce, sample = 'sample')
+
+
+# cell-level counts by sample
+#' @param sce SingleCellExperiment object with clonotype counts (sce$clono)
+#' @param by variable (name or vector) by which to split clonotype counts
+
+splitClonotypes <- function(sce, by){
+    if(is.null(sce$clono)){
+        stop('No clonotype counts found.')
+    }
+    if(length(by) == 1){
+        byVar <- factor(sce[[by]])
+    }else{
+        stopifnot(length(by) == ncol(sce))
+        byVar <- factor(by)
+    }
+    out <- lapply(sort(unique(byVar)), function(lv){
+        sce$clono[which(byVar == lv), ,drop = FALSE]
+    })
+    names(out) <- as.character(sort(unique(byVar)))
+    return(out)
+}
+
+# get sample level counts
+#' @param sce SingleCellExperiment object with clonotype counts (sce$clono)
+#' @param by variable (name or vector) by which to summarize clonotype counts
+
+summarizeClonotypes <- function(sce, by){
+    if(is.null(sce$clono)){
+        stop('No clonotype counts found.')
+    }
+    if(length(by) == 1){
+        byVar <- factor(sce[[by]])
+    }else{
+        stopifnot(length(by) == ncol(sce))
+        byVar <- factor(by)
+    }
+    out <- t(sapply(sort(unique(byVar)), function(lv){
+        colSums(sce$clono[which(byVar == lv), ,drop = FALSE])
+    }, USE.NAMES = TRUE))
+    rownames(out) <- as.character(sort(unique(byVar)))
+    return(out)
+}
+
+
+
+
+
+
+
