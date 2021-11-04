1e0aafaecc9973599ac02ac17dcf7a5450bdeb6f
Kelly Street <kstreet@root-pcs-MacBook-Air.local>
2021-11-04 14:41:32 -0400

    expanded summarizeClonotypes to work with matrix+factor, instead of just SCE

--- utils.R
+++ utils.R
@@ -25,27 +25,48 @@ splitClonotypes <- function(sce, by){
 }
 
 # get sample level counts
-#' @param sce SingleCellExperiment object with clonotype counts (sce$clono)
+#' @param x Matrix or SingleCellExperiment with clonotype counts (x$clono)
 #' @param by variable (name or vector) by which to summarize clonotype counts
+#' @param clonoCol when x is an SCE, the name for the clonotype matrix in the colData (default = 'clono')
+#' @return A table of sample-level clonotype counts
+require(Matrix)
 
-summarizeClonotypes <- function(sce, by){
-    if(is.null(sce$clono)){
-        stop('No clonotype counts found.')
-    }
-    if(length(by) == 1){
-        byVar <- factor(sce[[by]])
-    }else{
-        stopifnot(length(by) == ncol(sce))
-        byVar <- factor(by)
-    }
-    out <- t(sapply(sort(unique(byVar)), function(lv){
-        colSums(sce$clono[which(byVar == lv), ,drop = FALSE])
-    }, USE.NAMES = TRUE))
-    rownames(out) <- as.character(sort(unique(byVar)))
-    return(out)
-}
+setGeneric(name = "summarizeClonotypes",
+           signature = c("x","by"),
+           def = function(x, by, ...) standardGeneric("summarizeClonotypes"))
 
+setMethod(f = "summarizeClonotypes",
+          signature = signature(x = "SingleCellExperiment"),
+          definition = function(x, by, clonoCol = 'clono'){
+              if(is.null(x[[clonoCol]])){
+                  stop('No clonotype counts found.')
+              }
+              if(length(by) == 1){
+                  byVar <- factor(x[[by]])
+              }else{
+                  byVar <- factor(by)
+              }
+              out <- summarizeClonotypes(x[[clonoCol]], byVar)
+              return(out)
+          })
 
+setMethod(f = "summarizeClonotypes",
+          signature = signature(x = "matrix"),
+          definition = function(x, by){
+              summarizeClonotypes(Matrix(x), by)
+          })
+
+setMethod(f = "summarizeClonotypes",
+          signature = signature(x = "Matrix"),
+          definition = function(x, by){
+              stopifnot(length(by) == nrow(x))
+              by <- factor(by)
+              out <- t(sapply(sort(unique(by)), function(lv){
+                  colSums(x[which(by == lv), ,drop = FALSE])
+              }, USE.NAMES = TRUE))
+              rownames(out) <- as.character(sort(unique(by)))
+              return(out)
+          })
 
 
 
