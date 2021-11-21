46dbff77ab5eb637643e31f4c04155af72626149
Kelly Street <kstreet@root-pcs-MacBook-Air.local>
2021-11-21 15:24:12 -0500

    added documentation to utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,26 +1,69 @@
 
 # cell-level counts by sample
-#' @param sce SingleCellExperiment object with clonotype counts (sce$clono)
-#' @param by variable (name or vector) by which to split clonotype counts
+#' @title Split cell-level clonotype counts by sample
+#' @name splitClonotypes
+#' @export
+setGeneric(name = "splitClonotypes",
+           signature = c("x","by"),
+           def = function(x, by, ...) standardGeneric("splitClonotypes"))
 
-splitClonotypes <- function(sce, by){
-    if(is.null(sce$clono)){
-        stop('No clonotype counts found.')
-    }
-    if(length(by) == 1){
-        byVar <- factor(sce[[by]])
-    }else{
-        stopifnot(length(by) == ncol(sce))
-        byVar <- factor(by)
-    }
-    out <- lapply(sort(unique(byVar)), function(lv){
-        sce$clono[which(byVar == lv), ,drop = FALSE]
-    })
-    names(out) <- as.character(sort(unique(byVar)))
-    return(out)
-}
+#' @rdname splitClonotypes
+#' 
+#' @description Takes a matrix of cell-level clonotype counts and splits them
+#'   into a list of group-specific counts (typically samples).
+#' 
+#' @param x A \code{Matrix} of cell-level clonotype assignments
+#'   (cells-by-clonotypes) or a \code{SingleCellExperiment} object with such a
+#'   matrix stored in the \code{clono} slot of the \code{colData}.
+#' @param by A character vector or factor by which to split the clonotype
+#'   counts. If \code{x} is a \code{SingleCellExperiment} object, this can also
+#'   be a character, giving the name of the column from the \code{colData} to
+#'   use as this variable.
+#'   
+#' @return A list of \code{Matrix} objects providing the cell-level counts for
+#'   each unique value of \code{by} (if \code{by} denotes sample labels, each
+#'   matrix in the list will contain the cells from a single sample).
+#' 
+#' @import Matrix
+#' @export
+setMethod(f = "splitClonotypes",
+          signature = signature(x = "Matrix"),
+          definition = function(x, by){
+              stopifnot(length(by) == nrow(x))
+              out <- lapply(sort(unique(byVar)), function(lv){
+                  x[which(byVar == lv), ,drop = FALSE]
+              })
+              names(out) <- as.character(sort(unique(byVar)))
+              return(out)
+          })
 
 
+#' @rdname splitClonotypes
+#' @import Matrix
+#' @export
+setMethod(f = "splitClonotypes",
+          signature = signature(x = "matrix"),
+          definition = function(x, by){
+              splitClonotypes(Matrix(x), by)
+          })
+
+#' @rdname splitClonotypes
+#' @import SingleCellExperiment
+#' @export
+setMethod(f = "splitClonotypes",
+          signature = signature(x = "SingleCellExperiment"),
+          definition = function(x, by, clonoCol = 'clono'){
+              if(is.null(x$clono)){
+                  stop('No clonotype counts found.')
+              }
+              if(length(by) == 1){
+                  byVar <- factor(x[[by]])
+              }else{
+                  stopifnot(length(by) == ncol(x))
+                  byVar <- factor(by)
+              }
+              return(splitClonotypes(x$clono, byVar))
+          })
 
 
 #' @title Get sample-level clonotype counts
